9fe1b3ca77a6f4f53aafecb6b87e7cfe0e4b01c1
ZeroJ <854136959@qq.com>
2016-03-25 18:09:51 +0800

    Initial Commit

---
+++ TypeTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TypeTableView
+//
+//  Created by jasnig on 16/3/25.
+//  Copyright © 2016年 ZeroJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
